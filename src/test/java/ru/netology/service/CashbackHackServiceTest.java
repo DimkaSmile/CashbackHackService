d75ca214eddadacd87531e13f2b14883f4566445
DimkaSmile <RUS.mumik@gmail.com>
2022-08-01 21:46:40 +0300

    junit4 commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,8 +1,10 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
+//import org.testng.annotations.Test;
 
-import static org.testng.AssertJUnit.assertEquals;
+import static org.junit.Assert.assertEquals;
+//import static org.testng.AssertJUnit.assertEquals;
 
 public class CashbackHackServiceTest {
     CashbackHackService cashbackHackService = new CashbackHackService();
@@ -10,37 +12,54 @@ public class CashbackHackServiceTest {
     @Test
     public void shouldCashbackHackService() {
 
-        int actual = cashbackHackService.remain(900);
         int expected = 100;
+        int actual = cashbackHackService.remain(900);
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
     public void shouldCashbackHackService2() {
 
-        int actual = cashbackHackService.remain(800);
         int expected = 200;
+        int actual = cashbackHackService.remain(800);
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
     public void shouldCashbackHackService3() {
 
-        int actual = cashbackHackService.remain(999);
         int expected = 1;
+        int actual = cashbackHackService.remain(999);
 
-        assertEquals(actual, expected);
+
+        assertEquals(expected, actual);
     }
 
     @Test
     public void shouldCashbackHackService4() {
 
-        int actual = cashbackHackService.remain(0);
         int expected = 1000;
+        int actual = cashbackHackService.remain(0);
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldCashbackHackService5() {
+
+        int expected = 1000;
+        int actual = cashbackHackService.remain(1000);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldCashbackHackService6() {
+
+        int expected = 999;
+        int actual = cashbackHackService.remain(1);
+
+        assertEquals(expected, actual);
     }
     @Test
     public void shouldCashbackHackService7() {
